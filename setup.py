653051be421edb87e4684e5092d7231c2e847554
Omer Katz <omer.drow@gmail.com>
2013-02-08 19:17:09 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django_vagrant',
+    version='0.1',
+    description="",
+    author="Omer Katz",
+    author_email='omer.drow@gmail.com',
+    url='https://github.com/thedrow/django-vagrant',
+)
