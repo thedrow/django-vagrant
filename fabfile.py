bd57e75a1b967857e7ddc22d90d027c6acb62890
Omer Katz <omer.drow@gmail.com>
2013-02-08 22:19:03 +0300

    Added a basic layout for the box template.

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,13 @@
+from shutil import copyfile, move
+from fabric.context_managers import cd
+from fabric.decorators import task
+from fabric.operations import local
 import tests
+
+@task
+def package():
+    with cd('./box_template'):
+        copyfile('Vagrantfile', 'Vagrantfile.pkg')
+        local('vagrant up')
+        local('vagrant package --vagrantfile Vagrantfile.pkg')
+        move('./django_devenv.box', '../boxes/django_devenv.box')
