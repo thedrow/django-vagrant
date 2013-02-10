30ffd69fae6f575b63b03ad4919cc122cf0f9b33
Omer Katz <omer.drow@gmail.com>
2013-02-10 19:28:50 +0300

    Implemented the Role class constructor. All tests pass correctly

--- vagrant/config.py
+++ vagrant/config.py
@@ -1,3 +1,13 @@
+import os
+
+
 class Role(object):
     def __init__(self, name, box_path):
-        pass
+        if not os.path.isfile(box_path):
+            raise ValueError('Path to box file does not exist.')
+
+        if os.path.splitext(box_path)[1] != '.box':
+            raise ValueError("Invalid filename. Filename extension must be '.box'.")
+
+        self.name = name
+        self.box_path = box_path
